deed453704072842c990a75e21a42555d73449f9
jmpl10 <jmpl@educa.madrid.org>
2026-05-27 10:19:14 +0200

    Scanner atributo en Main. Parcial prestamo libro

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -8,12 +8,10 @@ import java.util.List;
 public class Biblioteca {
     private List<Libro> libros;
     private Map<String, Persona> personas;
-
     Biblioteca(){
         libros=new ArrayList<>();
         personas=new HashMap<>();
     }
-
     public void anadirLibro(Libro l) {
         libros.add(l);
     }
@@ -24,7 +22,6 @@ public class Biblioteca {
         if (!personas.containsKey(nif))
           personas.put(nif,new Persona(nif,nombre));
     }
-
     public List<Usuario> listarUsuarios() {
         List<Usuario> listaUsuarios=new ArrayList<>();
         for (Persona p:personas.values()){
@@ -32,6 +29,22 @@ public class Biblioteca {
                 listaUsuarios.add((Usuario)p);
         }
         return listaUsuarios;
+    }
+    public Usuario getUsuario(String nif) {
+        Persona p= personas.get(nif);
+        if (p!=null && p instanceof Usuario)
+          return (Usuario)p;
+        return null;
 
+    }
+
+    public Libro getLibro(int signatura) {
+        Libro laux=null;
+        for (Libro l:libros)
+            if (l.getSignatura()==signatura) {
+                laux = l;
+                break;
+            }
+        return laux;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     static void menu(Biblioteca b) {
-        Scanner sc = new Scanner(System.in);
         int opc=0;
         System.out.println("Biblioteca Principal");
         do {
@@ -27,10 +27,28 @@ public class Main {
                 case 2-> altaUsuario(b);
                 case 3-> listarLibros(b);
                 case 4-> listarUsuarios(b);
+                case 5-> prestarLibros(b);
+                case 6-> devolverLibro(b);
+                case 7-> listarLibroPrestado(b); //indicar los usuarios que tienen un libro dado
                 case 0-> System.out.println("Fin");
                 default -> System.out.println("Error. Opción no válida");
             }
         }while (opc!=0);
+    }
+    private static void prestarLibros(Biblioteca b) {
+        System.out.println("nif usuario");
+        String nif=sc.nextLine();
+        Usuario u= b.getUsuario(nif);
+        if (u!=null){
+            System.out.println("signatura libro");
+            int signatura=Integer.parseInt(sc.nextLine());
+            Libro l=b.getLibro(signatura);
+            if (l!=null){
+                b.prestarLibro(u, l);  //decrementar ejemplares disponibles
+            }
+        }
+        else System.out.println("Usuario no existe");
+
     }
 
     private static void listarUsuarios(Biblioteca b) {
@@ -38,38 +56,30 @@ public class Main {
         for (Usuario u:listaUsuarios){
             System.out.println(u);
         }
+        //deben mostrarse los libros prestados
     }
-
     private static void altaUsuario(Biblioteca b) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("nif:");
         String nif = sc.nextLine();
         System.out.println("Nombre");
         String nombre = sc.nextLine();
         b.altaUsuario(nif,nombre);
-
     }
-
     private static void listarLibros(Biblioteca b) {
         List<Libro> listaLibros= b.todosLosLibros();
         for (Libro l:listaLibros)
             System.out.println(l);
     }
-
     private static void altaLibro(Biblioteca b) {
-        Scanner sc= new Scanner(System.in);
         System.out.println("Título");
         String titulo = sc.nextLine();
         System.out.println("Num Ejemplares");
         int numEjemplares = sc.nextInt();
         Libro l= new Libro(titulo, numEjemplares);
         b.anadirLibro(l);
-
     }
-
     public static void main(String[] args) {
             Biblioteca b=new Biblioteca();
             menu(b);
-
     }
 }
